df27ff92d2a51d5fa2ba9d860f0776d3dfbf13a7
Semyen747 <combatpilot45@gmail.com>
2023-07-07 22:59:24 +0300

    timeouts

--- 7.4/puppeteer/gh.test.js
+++ 7.4/puppeteer/gh.test.js
@@ -2,7 +2,6 @@ let page;
 
 beforeEach(async () => {
   page = await browser.newPage();
-  await page.goto("https://github.com/team");
 });
 
 afterEach(() => {
@@ -10,25 +9,34 @@ afterEach(() => {
 });
 
 describe("Github page tests", () => {
+  beforeEach(async () => {
+    await page.goto("https://github.com/team");
+  });
+
+
   test("The h1 header content'", async () => {
     const firstLink = await page.$("header div div a");
     await firstLink.click();
-    await page.waitForSelector('h1');
+    await page.waitForSelector("h1");
     const title2 = await page.title();
-    expect(title2).toEqual('GitHub: Where the world builds software · GitHub');
-  });
+    expect(title2).toEqual("GitHub: Where the world builds software · GitHub");
+  }, 5000);
 
   test("The first link attribute", async () => {
-    const actual = await page.$eval("a", link => link.getAttribute('href') );
+    const actual = await page.$eval("a", (link) => link.getAttribute("href"));
     expect(actual).toEqual("#start-of-content");
-  });
+  }, 5000);
 
   test("The page contains Sign in button", async () => {
     const btnSelector = ".btn-large-mktg.btn-mktg";
     await page.waitForSelector(btnSelector, {
       visible: true,
     });
-    const actual = await page.$eval(btnSelector, link => link.textContent);
-    expect(actual).toContain("Sign up for free")
-  });
+    const actual = await page.$eval(btnSelector, (link) => link.textContent);
+    expect(actual).toContain("Sign up for free");
+  }, 5000);
 });
+
+
+
+
