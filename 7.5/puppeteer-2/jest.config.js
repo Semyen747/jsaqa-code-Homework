1a2ad6ed88f0a970970c97a99fef37a0a521da3a
Semyen747 <combatpilot45@gmail.com>
2023-07-11 04:15:41 +0300

    part 2

--- 7.5/puppeteer-2/jest.config.js
+++ 7.5/puppeteer-2/jest.config.js
@@ -2,4 +2,8 @@ module.exports = {
     verbose: true, //указывает на то, что каждый тест будет показан в отчете о процессе запуска 
      preset: "jest-puppeteer", //указываем, что будем использовать эту библиотеку
      testTimeout: 65500, // вернул таймаут по рекомендации программы
+	 testMatch: ['<rootDir>/features/**/*.steps.js'],
+  transform: {
+    '^.+\\.steps.js$': 'jest-cucumber'
+  }
  };
